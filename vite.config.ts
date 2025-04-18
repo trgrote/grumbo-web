996fbb05692effb6882a1a19408fa4664aea0053
trgrote <grotese@gmail.com>
2025-04-18 12:29:04 -0500

    Add Aliases for paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from "path";
 
 // https://vite.dev/config/
+// https://dev.to/tilly/aliasing-in-vite-w-typescript-1lfo
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+		},
+	},
+});
