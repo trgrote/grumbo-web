ecb981d231a66366005ab6937a466f1a6f3af567
trgrote <grotese@gmail.com>
2025-07-08 17:59:08 -0500

    Align home page elments

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,13 +2,15 @@ import professional from '@assets/professional.jpg';
 
 function Home() {
 	return (
-		<>
-			<h1>Grumbo Professional Enterprises</h1>
-			<img src={professional} />
-			<p className="read-the-docs">
-				We make business, matter
-			</p>
-		</>
+		<div className='h-screen flex justify-center items-center'>
+			<div className='text-center'>
+				<h1>Grumbo Professional Enterprises</h1>
+				<img src={professional} />
+				<p className="read-the-docs">
+					We make business, matter
+				</p>
+			</div>
+		</div>
 	);
 };
 
