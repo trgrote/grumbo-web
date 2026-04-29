2aad99d3023cee7ca7b28a0d4cd8a03d473d7463
trgrote <grotese@gmail.com>
2026-04-28 20:04:28 -0500

    Add Tabbed view for gloomstalker page

--- src/pages/gloomstalker/GloomStalkerPage.tsx
+++ src/pages/gloomstalker/GloomStalkerPage.tsx
@@ -1,9 +1,45 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState, useEffect } from "react";
+import { GloomStalkerInfo } from "./GloomStalkerTypes";
+import { GetLocalGloomStalkerStorage, SaveLocalGloomStalkerStorage } from "./GloomStalkerLocalStorage";
 
 function GloomStalkerPage() {
+	const [gloomStalkerInfo/*, setGloomStalkerInfo*/] = useState<GloomStalkerInfo>(() => {
+		const localStorage = GetLocalGloomStalkerStorage();
+		return localStorage.gloomStalkerInfo;
+	});
+
+	// const [attackResults, setAttackResults] = useState<RollHistoryRecord[]>(() => {
+	// 	const localStorage = GetLocalGloomStalkerStorage();
+	// 	return localStorage.attackResults;
+	// });
+
+	// On State change, save results to local storage
+	useEffect(() => {
+		SaveLocalGloomStalkerStorage({
+			gloomStalkerInfo: gloomStalkerInfo,
+			// attackResults
+		});
+	}, [gloomStalkerInfo/*, attackResults*/]);
+
 	return (
-		<div>
-			<h1>Gloom Stalker</h1>
-		</div>
+		<Tabs defaultValue="info">
+			<TabsList>
+				<TabsTrigger value="info">Gloom Stalker Info</TabsTrigger>
+				<TabsTrigger value="history">History</TabsTrigger>
+			</TabsList>
+			<TabsContent value="info">
+				{/* <GloomStalkerInfoTab
+					gloomStalkerInfo={gloomStalkerInfo}
+					onChange={setGloomStalkerInfo}
+					addToRollHistory={roll => setAttackResults([roll, ...attackResults])} /> */}
+			</TabsContent>
+			<TabsContent value="history">
+				{/* <GloomStalkerHistoryTab attackResults={attackResults}
+					rollRecordRenderer={(attackResult) => <RollResultView {...attackResult} />}
+					onClearHistory={() => setAttackResults([])} /> */}
+			</TabsContent>
+		</Tabs>
 	);
 }
 
