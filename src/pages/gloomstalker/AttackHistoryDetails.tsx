90f918b2d9f6dfdda186221894eccff3e5e9c637
trgrote <grotese@gmail.com>
2026-05-07 20:40:39 -0500

    Move Attack History Details to own component

---
+++ src/pages/gloomstalker/AttackHistoryDetails.tsx
@@ -0,0 +1,85 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Checkbox } from "@/components/ui/checkbox";
+import { GetHitRollStatus } from "./AttackSheet/AttackSheetStateFunctions";
+import { HistoryRecord, HitRollStatus } from "./GloomStalkerTypes";
+
+const rollArrayToString = (arr: number[]) => '[' + arr.join(', ') + ']';
+const diceArrayToString = (arr: number[]) => '[' + arr.map(die => `d${die}`).join(', ') + ']';
+
+export default function AttackHistoryDetails({ historyRecord }: { historyRecord: HistoryRecord; }) {
+	const { gloomStalkerInfo } = historyRecord;
+
+	const hitStatus = GetHitRollStatus(historyRecord.attackRolls);
+	const totalPiercingDamage = historyRecord.piercingDamageRolls.reduce((a, value) => a + value, 0)
+		+ gloomStalkerInfo.damageModifier
+		+ (historyRecord.applySharpShooterPenalty ? 10 : 0);
+	const totalFireDamage = historyRecord.fireDamageRolls.reduce((a, value) => a + value, 0);
+	const totalDamage = totalPiercingDamage + totalFireDamage;
+
+	return (
+		<Card>
+			<CardContent>
+				<ul>
+					{historyRecord.isHit &&
+						<>
+							<li>
+								<Label>Total Damage: {totalDamage}</Label>
+							</li>
+							<li>
+								<Label>Total Piercing Damage: {totalPiercingDamage}</Label>
+							</li>
+							<li>
+								<Label>Total Fire Damage: {totalFireDamage}</Label>
+							</li>
+							<li>
+								<Label>Apply 5 Damage to Adjacent Enemies? <Checkbox disabled checked={hitStatus === HitRollStatus.CriticalHit} /></Label>
+							</li>
+						</>}
+					<li>
+						<Label>Attack Modifier: +{gloomStalkerInfo.attackModifier}</Label>
+					</li>
+					<li>
+						<Label>Weapon Stats: d{gloomStalkerInfo.damageDie} + {gloomStalkerInfo.damageModifier}</Label>
+					</li>
+					<li>
+						<Label>Had Advantage? <Checkbox disabled checked={historyRecord.hasAdvantage} /></Label>
+					</li>
+					<li>
+						<Label>Applied Sharp Shooter? <Checkbox disabled checked={historyRecord.applySharpShooterPenalty} /></Label>
+					</li>
+					<li>
+						<Label>To Hit Rolls: {rollArrayToString(historyRecord.attackRolls)}</Label>
+					</li>
+					<li>
+						<Label>Critical Hit? <Checkbox disabled checked={hitStatus === HitRollStatus.CriticalHit} /></Label>
+					</li>
+					<li>
+						<Label>Was Hit? <Checkbox disabled checked={historyRecord.isHit} /></Label>
+					</li>
+					{historyRecord.isHit &&
+						<>
+							<li>
+								<Label>Piercing Damage Dice: {diceArrayToString(historyRecord.piercingDamageDicePool)}</Label>
+							</li>
+							<li>
+								<Label>Piercing Damage Rolls: {rollArrayToString(historyRecord.piercingDamageRolls)}</Label>
+							</li>
+							<li>
+								<Label>Fire Damage Dice: {diceArrayToString(historyRecord.fireDamageDicePool)}</Label>
+							</li>
+							<li>
+								<Label>Fire Damage Rolls: {rollArrayToString(historyRecord.fireDamageRolls)}</Label>
+							</li>
+							<li>
+								<Label>Dread Ambusher Extra Attack? <Checkbox disabled checked={historyRecord.isDreadAmbusherExtraAttack} /></Label>
+							</li>
+							<li>
+								<Label>Hunter's Mark? <Checkbox disabled checked={historyRecord.applyHuntersMark} /></Label>
+							</li>
+						</>}
+				</ul>
+			</CardContent>
+		</Card>
+	);
+};
--- src/pages/gloomstalker/AttackHistoryView.tsx
+++ src/pages/gloomstalker/AttackHistoryView.tsx
@@ -1,19 +1,14 @@
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
-import { HistoryRecord, HitRollStatus } from "./GloomStalkerTypes";
-import { Card, CardContent } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
-import { Checkbox } from "@/components/ui/checkbox";
+import { HistoryRecord } from "./GloomStalkerTypes";
 import { Button } from "@/components/ui/button";
 import { ChevronDown } from "lucide-react";
 import { GetHitRollStatus, GetHitRollStatusColorClass } from "./AttackSheet/AttackSheetStateFunctions";
+import AttackHistoryDetails from "./AttackHistoryDetails";
 
 interface AttackHistoryViewProps {
 	defaultOpen?: boolean;
 	historyRecord: HistoryRecord;
 }
-
-const rollArrayToString = (arr: number[]) => '[' + arr.join(', ') + ']';
-const diceArrayToString = (arr: number[]) => '[' + arr.map(die => `d${die}`).join(', ') + ']';
 
 export default function AttackHistoryView({ defaultOpen, historyRecord }: AttackHistoryViewProps) {
 	const { gloomStalkerInfo } = historyRecord;
@@ -35,6 +30,7 @@ export default function AttackHistoryView({ defaultOpen, historyRecord }: Attack
 			<CollapsibleTrigger asChild className="w-full">
 				<Button variant="ghost">
 					<h3>
+						{new Date(historyRecord.timestamp).toLocaleString()} -
 						To Hit: <span className={hitValueTextColorClass}>{totalHitRoll}</span>&nbsp;
 						Damage: <span className="text-red-500">{historyRecord.isHit ? totalDamage : 0}</span>
 					</h3>
@@ -42,72 +38,7 @@ export default function AttackHistoryView({ defaultOpen, historyRecord }: Attack
 				</Button>
 			</CollapsibleTrigger>
 			<CollapsibleContent>
-				<Card>
-					<CardContent>
-						<ul>
-							{historyRecord.isHit &&
-								<>
-									<li>
-										<Label>Total Damage: {totalDamage}</Label>
-									</li>
-									<li>
-										<Label>Total Piercing Damage: {totalPiercingDamage}</Label>
-									</li>
-									<li>
-										<Label>Total Fire Damage: {totalFireDamage}</Label>
-									</li>
-									<li>
-										<Label>Apply 5 Damage to Adjacent Enemies? <Checkbox disabled checked={hitStatus === HitRollStatus.CriticalHit} /></Label>
-									</li>
-								</>
-							}
-							<li>
-								<Label>Attack Modifier: +{gloomStalkerInfo.attackModifier}</Label>
-							</li>
-							<li>
-								<Label>Weapon Stats: d{gloomStalkerInfo.damageDie} + {gloomStalkerInfo.damageModifier}</Label>
-							</li>
-							<li>
-								<Label>Had Advantage? <Checkbox disabled checked={historyRecord.hasAdvantage} /></Label>
-							</li>
-							<li>
-								<Label>Applied Sharp Shooter? <Checkbox disabled checked={historyRecord.applySharpShooterPenalty} /></Label>
-							</li>
-							<li>
-								<Label>To Hit Rolls: {rollArrayToString(historyRecord.attackRolls)}</Label>
-							</li>
-							<li>
-								<Label>Critical Hit? <Checkbox disabled checked={hitStatus === HitRollStatus.CriticalHit} /></Label>
-							</li>
-							<li>
-								<Label>Was Hit? <Checkbox disabled checked={historyRecord.isHit} /></Label>
-							</li>
-							{
-								historyRecord.isHit &&
-								<>
-									<li>
-										<Label>Piercing Damage Dice: {diceArrayToString(historyRecord.piercingDamageDicePool)}</Label>
-									</li>
-									<li>
-										<Label>Piercing Damage Rolls: {rollArrayToString(historyRecord.piercingDamageRolls)}</Label>
-									</li>
-									<li>
-										<Label>Fire Damage Dice: {diceArrayToString(historyRecord.fireDamageDicePool)}</Label>
-									</li>
-									<li>
-										<Label>Fire Damage Rolls: {rollArrayToString(historyRecord.fireDamageRolls)}</Label>
-									</li>
-									<li>
-										<Label>Dread Ambusher Extra Attack? <Checkbox disabled checked={historyRecord.isDreadAmbusherExtraAttack} /></Label>
-									</li>
-									<li>
-										<Label>Hunter's Mark? <Checkbox disabled checked={historyRecord.applyHuntersMark} /></Label>
-									</li>
-								</>
-							}
-						</ul>
-					</CardContent>
-				</Card>
+				<AttackHistoryDetails historyRecord={historyRecord} />
 			</CollapsibleContent>
 		</Collapsible>
 	);
