6aa492e17b223eadd46d6465c2f0ea16377e2039
trgrote <grotese@gmail.com>
2026-05-08 19:03:18 -0500

    Simplify History Title to just include timestamp and if it was a hit or not

--- src/pages/gloomstalker/AttackHistoryView.tsx
+++ src/pages/gloomstalker/AttackHistoryView.tsx
@@ -2,7 +2,6 @@ import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/component
 import { HistoryRecord } from "./GloomStalkerTypes";
 import { Button } from "@/components/ui/button";
 import { ChevronDown } from "lucide-react";
-import { GetHitRollStatus, GetHitRollStatusColorClass } from "./AttackSheet/AttackSheetStateFunctions";
 import AttackHistoryDetails from "./AttackHistoryDetails";
 
 interface AttackHistoryViewProps {
@@ -11,19 +10,8 @@ interface AttackHistoryViewProps {
 }
 
 export default function AttackHistoryView({ defaultOpen, historyRecord }: AttackHistoryViewProps) {
-	const { gloomStalkerInfo } = historyRecord;
-
-	const highestHitRoll = Math.max(...historyRecord.attackRolls);
-	const hitStatus = GetHitRollStatus(historyRecord.attackRolls);
-	const totalHitRoll = highestHitRoll
-		+ gloomStalkerInfo.attackModifier
-		- (historyRecord.applySharpShooterPenalty ? 5 : 0);
-	const totalPiercingDamage = historyRecord.piercingDamageRolls.reduce((a, value) => a + value, 0)
-		+ gloomStalkerInfo.damageModifier
-		+ (historyRecord.applySharpShooterPenalty ? 10 : 0);
-	const totalFireDamage = historyRecord.fireDamageRolls.reduce((a, value) => a + value, 0);
-	const totalDamage = totalPiercingDamage + totalFireDamage;
-	const hitValueTextColorClass = GetHitRollStatusColorClass(hitStatus);
+	const hitStatusText = historyRecord.isHit ? " Hit" : " Miss";
+	const hitValueTextColorClass = historyRecord.isHit ? "text-green-500" : "text-red-500";
 
 	return (
 		<Collapsible defaultOpen={defaultOpen ?? false} className="group/collapsible">
@@ -31,8 +19,7 @@ export default function AttackHistoryView({ defaultOpen, historyRecord }: Attack
 				<Button variant="ghost">
 					<h3>
 						{new Date(historyRecord.timestamp).toLocaleString()} -
-						To Hit: <span className={hitValueTextColorClass}>{totalHitRoll}</span>&nbsp;
-						Damage: <span className="text-red-500">{historyRecord.isHit ? totalDamage : 0}</span>
+						<span className={hitValueTextColorClass}>{hitStatusText}</span>
 					</h3>
 					<ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
 				</Button>
